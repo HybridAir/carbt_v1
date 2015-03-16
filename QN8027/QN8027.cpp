c99bf05c7b842e528ba745c08ceb3f76106e80b2
'HybridAir' <s.tratton.blake@gmail.com>
2015-03-16 16:15:42 -0700

    Added basic code

---
+++ QN8027/QN8027.cpp
@@ -0,0 +1,9 @@
+//this class manages the QN8027 device
+//lets see if I can do this
+//fuck
+
+#include "QN8027.h"
+
+QN8027::QN8027() {
+
+}
---
+++ QN8027/QN8027.h
@@ -0,0 +1,13 @@
+#ifndef QN8027_H
+#define	QN8027_H
+
+#include <mbed.h>
+
+class QN8027 {
+    public:
+		QN8027();
+    private:
+
+};
+
+#endif
